e08d83c9564fed7d7c430b064122e2d58b237ecf
William M. Riley <mikeriley20@gmail.com>
2016-06-26 18:27:09 -0500

    Initial Commit for Neighborhood

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+var slick = require('./helpers/_slick.js');
+var image_zoom = require('./helpers/_image-zoom.js');
+var pointer_events = require('./helpers/_pointer-events.js');
+var picturefill = require('./helpers/_picturefill.js');
+var apng_canvas = require('./helpers/_apng-canvas.js');
+var function_api = require('./helpers/_function_api.js');
+var pjax = require('./helpers/_pjax.js');
+var browser_fixes = require('./helpers/_browser-fixes.js');
+var router = require('./helpers/_router.js');
---
+++ partials/footer.php
@@ -0,0 +1,16 @@
+<footer class="content-info">
+	<div class="container bleed built-by-friends">
+		<div class="column">
+			<div class="content">
+				<p>Designed and built by your friends at <a href="http://aiganebraska.org" target="_blank"><img class="logo-footer" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-aiga-black.png" alt="AIGA Nebraska logo"></a></p>
+			</div>
+		</div>
+	</div>
+	<div class="container bleed hosted-flywheel">
+		<div class="column">
+			<div class="content">
+				<p>Hosted by <a href="http://getflywheel.com" target="_blank"><img class="flywheel" src="http://shownebraska.org/wp-content/themes/show_2015/images/flywheel-logo.svg" alt="Flywheel Hosting logo"></a></p>
+			</div>
+		</div>
+	</div>
+</footer>
